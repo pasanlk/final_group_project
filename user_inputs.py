e05f472829ff119eb46aeb50c3d8a86b2b43e5d2
SanjuZ30 <91816637+SanjuZ30@users.noreply.github.com>
2021-10-03 16:02:15 +0530

    Update user_inputs.py

--- user_inputs.py
+++ user_inputs.py
@@ -1,8 +1,6 @@
-import main
-import demo
 
 user_input = input("please enter required method(email or show_now); ")
-if user_input == "show_now":
-    main.print_df()
+if user_input == "show":
+    print_df()
 elif user_input == "email":
-    demo.send_mail()
+    send_mail()
